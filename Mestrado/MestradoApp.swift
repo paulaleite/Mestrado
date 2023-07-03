ed0513b1a3d863c127d3e116cf695630a3cd1604
paulaleite <paula.t.leite97@gmail.com>
2023-07-03 13:43:41 -0300

    Initial Commit

---
+++ Mestrado/MestradoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MestradoApp.swift
+//  Mestrado
+//
+//  Created by Paula Leite on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MestradoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
