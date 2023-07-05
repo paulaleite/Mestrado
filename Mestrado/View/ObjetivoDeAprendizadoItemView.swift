a06038fef551eca372bab9b344b71bd9ca42ecd4
paulaleite <paula.t.leite97@gmail.com>
2023-07-05 17:46:52 -0300

    Criar arquivos iniciais da View de Objetivo

---
+++ Mestrado/View/ObjetivoDeAprendizadoItemView.swift
@@ -0,0 +1,16 @@
+//
+//  ObjetivoDeAprendizadoItemView.swift
+//  Mestrado
+//
+//  Created by Paula Leite on 05/07/23.
+//
+
+import Foundation
+
+struct ObjetivoDeAprendizadoItemView: View {
+    var hexCorCompetencia: String
+    
+    var body: some View {
+        Text("Testando")
+    }
+}
