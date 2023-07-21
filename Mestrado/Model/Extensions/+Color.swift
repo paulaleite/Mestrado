fbe0835667ae94667eb2998097da697bbcac2234
paulaleite <paula.t.leite97@gmail.com>
2023-07-21 10:51:03 -0300

    Documentação do arquivo de extensão +Color

--- Mestrado/Model/Extensions/+Color.swift
+++ Mestrado/Model/Extensions/+Color.swift
@@ -9,6 +9,10 @@ import Foundation
 import SwiftUI
 
 extension Color {
+    // MARK: - Inicializadores
+    
+    /// Inicializador que permite que uma cor seja criada a partir de um array que contenha um código HEX. Não deve-se colocar a "#".
+    /// Exemplo: Color(hex: "ffffff")
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
@@ -34,50 +38,64 @@ extension Color {
         )
     }
     
+    // MARK: - Variáveis e Constantes
+    
+    /// Cor de ação primária do aplicativo.
     static var corDeAcao: Color? = {
         Color("Acao1")
     }()
     
+    /// Cor que representa a primeira competência dos objetivos de aprendizado.
     static var competencia1: Color? = {
        Color("Competencia1")
     }()
     
+    /// Cor que representa a segunda competência dos objetivos de aprendizado.
     static var competencia2: Color? = {
        Color("Competencia2")
     }()
     
+    /// Cor que representa a terceira competência dos objetivos de aprendizado.
     static var competencia3: Color? = {
        Color("Competencia3")
     }()
     
+    /// Cor que representa a quarta competência dos objetivos de aprendizado.
     static var competencia4: Color? = {
        Color("Competencia4")
     }()
     
+    /// Cor que representa o fundo primário do aplicativo.
     static var fundo1: Color? = {
        Color("Fundo1")
     }()
     
+    /// Cor que representa o fundo secundário do aplicativo.
     static var fundo2: Color? = {
        Color("Fundo2")
     }()
     
+    /// Cor que representa o fundo terciário do aplicativo.
     static var fundo3: Color? = {
        Color("Fundo3")
     }()
     
+    /// Cor que representa o fundo quaternário do aplicativo.
     static var fundo4: Color? = {
        Color("Fundo4")
     }()
     
+    /// Cor utilizada nos textos primários do aplicativo.
     static var texto1: Color? = {
        Color("Texto1")
     }()
     
+    /// Cor utilizada nos textos secundários do aplicativo.
     static var texto2: Color? = {
        Color("Texto2")
     }()
     
+    /// Cor utilizada nos textos terciários do aplicativo.
     static var texto3: Color? = {
        Color("Texto3")
     }()
