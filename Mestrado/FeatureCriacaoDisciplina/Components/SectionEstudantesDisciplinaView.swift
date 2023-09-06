eeabe373ce9500151e3c1be4b122ac4528bf1138
paulaleite <paula.t.leite97@gmail.com>
2023-09-06 17:47:26 -0300

    Estruturar a tela de estudantes

--- Mestrado/FeatureCriacaoDisciplina/Components/SectionEstudantesDisciplinaView.swift
+++ Mestrado/FeatureCriacaoDisciplina/Components/SectionEstudantesDisciplinaView.swift
@@ -8,7 +8,62 @@
 import SwiftUI
 
 struct SectionEstudantesDisciplinaView: View {
+    @EnvironmentObject var viewModel: PostDisciplinaViewModel
+    
+    @State var selecao: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Section {
+            NavigationLink {
+                EmptyView()
+            } label: {
+                Text("Adicionar Estudantes")
+                    .foregroundColor(Color.texto1)
+                    .font(.body)
+            }
+
+        }
+        
+        if viewModel.estudantes.isEmpty {
+            Section {
+                HStack {
+                    Toggle(isOn: $selecao) {
+                        Text("")
+                    }
+                    .toggleStyle(.button)
+                }
+            } header: {
+                HStack(spacing: 2) {
+                    Text("*")
+                        .foregroundColor(.red)
+                        .font(.system(size: 14))
+                        .fontWeight(.bold)
+                    
+                    Text("Titulo.Estudante.Plural".localized())
+                        .textCase(.uppercase)
+                        .font(.system(size: 14))
+                        .foregroundColor(Color.texto2)
+                    
+                    Spacer()
+                }
+            }
+        }
+
+//        header: {
+//            HStack(spacing: 2) {
+//                Text("*")
+//                    .foregroundColor(.red)
+//                    .font(.system(size: 14))
+//                    .fontWeight(.bold)
+//
+//                Text("Titulo.Estudante.Plural".localized())
+//                    .textCase(.uppercase)
+//                    .font(.system(size: 14))
+//                    .foregroundColor(Color.texto2)
+//
+//                Spacer()
+//            }
+//        }
+
     }
 }
