c81edfc7d0448b0508426e61c983538da3cb01b4
paulaleite <paula.t.leite97@gmail.com>
2023-09-08 14:32:13 -0300

    Agrupar os objetivos pelas categorias, bem como puxar dados necessários.

--- Mestrado/FeatureCriacaoDisciplina/Components/EscolherObjetivosMomentoView.swift
+++ Mestrado/FeatureCriacaoDisciplina/Components/EscolherObjetivosMomentoView.swift
@@ -12,8 +12,45 @@ struct EscolherObjetivosMomentoView: View {
     // MARK: - Variáveis e Constantes
     @EnvironmentObject var viewModel: PostDisciplinaViewModel
     
+    /// Texto da busca que vai ser atualizado sempre que o usuário mudá-lo.
+    @State var textoBusca: String = ""
+    
+    var resultadoBusca: [ObjetivoGetPostDisciplinaModel] {
+        if textoBusca.isEmpty {
+            return viewModel.objetivosDeAprendizadoDisponiveis
+        } else {
+            return viewModel.objetivosDeAprendizadoDisponiveis.filter {
+                $0.descricao.contains(textoBusca)
+            }
+        }
+    }
+    
     // MARK: - Body da View
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(agruparPorCategoria(viewModel.objetivosDeAprendizadoDisponiveis), id: \.0) { par in
+                Section {
+                    ForEach(par.1, id: \.self) { obj in
+                        Text(obj.descricao)
+                    }
+                } header: {
+                    Text(par.0)
+                        .font(.system(size: 11))
+                        .foregroundColor(Color.texto2)
+                }
+            }
+        }
+        .searchable(text: $textoBusca)
+        .onAppear {
+            Task {
+                await viewModel.getDadosCriacaoDisciplina()
+            }
+        }
+    }
+    
+    // MARK: - Funçõoes
+    func agruparPorCategoria(_ objetivos: [ObjetivoGetPostDisciplinaModel]) -> [(String, [ObjetivoGetPostDisciplinaModel])] {
+        let agrupado = Dictionary(grouping: objetivos, by: { $0.competencia })
+        return agrupado.sorted(by: { $0.key < $1.key })
     }
 }
